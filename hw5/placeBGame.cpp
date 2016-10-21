bf68de9e5ad94b1261d4005d4801ad64b2cb7f60
KiraKing <khoile315@gmail.com>
2016-10-21 14:18:07 -0500

    commit 2 hw5

--- hw5/placeBGame.cpp
+++ hw5/placeBGame.cpp
@@ -3,62 +3,62 @@
 /* Function: Make robot find bomb*/
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-    int main() {
-        srand(static_cast <unsigned int> (time(0)));
-        int botGuess = rand() % 99+2+1; 
-        int max = 99;
-        int min = 2;
-        char ready;
-        char highLowSuccess;
-        bool success;
-        int tries = 0;  
-        cout << "Place bomb in room number 1 - 100!\n";
-        cout << "Are you ready? (y/n)\n\n";
-        cin >> ready;
+int main() {
+    srand(static_cast <unsigned int> (time(NULL)));
+	int max = 99;
+    int min = 2;
+    int botGuess = rand() % (max - min + 1 ) +min;
+    char ready = 'y';
+    char highLowSuccess;
+    bool success;
+    int tries = 0;
+    int maxTries;
+	bool repeat = true;
+	
+	while (repeat = true) {
+		cout << "Are you ready? (y/n)\n";
+		cin >> ready;
+		if (ready == 'n'){
+			break;
+		}
+		cout << "Place bomb in room number 1 - 100!\n";
+		cout << "How many tries do I get?\n";
+		cin >> maxTries;
+
         while (ready == 'y') {
             cout << "Is it in room #" << botGuess << "?\n\n";
-            cout << "Was "<< botGuess << ":(h)igh, (l)ow or (c)orrect? (h/l/c): ";
+            cout << "Is room "<< botGuess << ":(h)igh, (l)ow or (c)orrect? (h/l/c): ";
             ++tries;
-            cin >> highLowSuccess;                                   
+            cin >> highLowSuccess;
             if (highLowSuccess == 'h' && botGuess != 2) {
-                max = botGuess - 1;                                 
-                botGuess = rand() % max +1;                        
-                success = false;
+                max = botGuess - 1;
+                botGuess = rand() % max +1;
             }
-			else if (highLowSuccess == 'h' && botGuess == 2) {
-                cout << "Its must be in room 1!\nThat only took me " << tries << " tries.\n";
-                success = true;
+            else if (highLowSuccess == 'h' && botGuess == 2) {
+                cout << "Its must be in room 1!\nThat only took me " << tries << " tries.\n\n";
 				break;
-				ready == 'n';    
-				cout << "Please pick a number between 1 - 100.\n\n";
-				cout << "Replay? (y/n)\n\n";
-				cin >> ready;				
             }
-            else if (highLowSuccess == 'l' && botGuess != 99) {   
-                min = botGuess + 1;                                 
+            else if (highLowSuccess == 'l' && botGuess != 99) {
+                min = botGuess + 1;
                 botGuess = rand()%(max-min + 1) + min;;
-                success = false;
             }
-			else if (highLowSuccess == 'l' && botGuess == 99) {   
-                cout << "Its must be in room 100!\nThat only took me " << tries << " tries.\n";
-                success = true;
+            else if (highLowSuccess == 'l' && botGuess == 99) {
+                cout << "Its must be in room 100!\nThat only took me " << tries << " tries.\n\n";
 				break;
-				ready == 'n';
-				cout << "Please pick a number between 1 - 100.\n\n";
-				cout << "Replay? (y/n)\n\n";
-				cin >> ready;
             }
-            else if (highLowSuccess == 'c') {                        
+            else if (highLowSuccess == 'c') {
                 cout << "That only took me " << tries << " tries.\n";
-                success = true;
-				ready == 'n';
-				cout << "Please pick a number between 1 - 100.\n\n";
-				cout << "Replay? (y/n)\n\n";
-				cin >> ready;
-			}
+                break;
+            }
+			if (tries == maxTries) {
+                cout << "I'm out of tries, I GIVE UP!\n";
+                break;
+            }
 		}
-		return 0;
-    }
+	}
+    return 0;
+}
